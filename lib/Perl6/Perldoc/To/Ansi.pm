458bdee29841f208df141529a2adb3dca506d9a4
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2009-05-28 16:09:44 +0000

    Bump version to 0.02

--- lib/Perl6/Perldoc/To/Ansi.pm
+++ lib/Perl6/Perldoc/To/Ansi.pm
@@ -3,7 +3,7 @@ package Perl6::Perldoc::To::Ansi;
 use warnings;
 use strict;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 # add fake opening/closing tags, to be processed later
 sub add_ansi {
